4072f152ecfc8f7aa4d1186c7476757a20fc1128
Dennis Henrik Kristensen <Dennishk@protonmail.com>
2021-02-04 12:30:46 +0100

    Models
    * Added a few models and added properties for the appuser
    * Checkpoint here for testing to get appuser in the "correct" folder try again

--- Biblio/Server/Models/ApplicationUser.cs
+++ Biblio/Server/Models/ApplicationUser.cs
@@ -8,5 +8,23 @@ namespace Biblio.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return Firstname + " " + Lastname;
+            }
+        }
+        public DateTime? DOB { get; set; }
+
+        // Relations
+        [ForeignKey("LibraryId")]
+        public int? HomeLibraryId { get; set; }
+        public Library HomeLibrary { get; set; }
+
+        public ICollection<BookCopy> BorrowedBooks { get; set; }
+        public ICollection<BorrowedBookHistory> BorrowedBookHistories { get; set; }
     }
 }
---
+++ Biblio/Shared/Models/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio.Shared.Models
+{
+    /// <summary>
+    /// This model is for a shared understanding of what a book is. A book is unique with format incl. this means that multiple physical books still refers to the same instance of this model/ row in the database. 
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// Booksformats supported.
+        /// </summary>
+        public enum BookFormats
+        {
+            Hardcover,
+            Softcover,
+            Paperback,
+            Folded
+        }
+
+        public enum BookTypes
+        {
+            Fiction,
+            NonFiction,
+        }
+
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Blurb { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public short? Height { get; set; }
+        public short? Depth { get; set; }
+        public short? Width { get; set; }
+        public short? Weight { get; set; }
+        public short NumberofPages { get; set; }
+        public byte[]? Image { get; set; }
+        public BookFormats Format { get; set; }
+        public BookTypes Type { get; set; }
+
+        // Relations
+        public ICollection<Author> Authors { get; set; }
+        public ICollection<Genre> Genres { get; set; }
+    }
+}
