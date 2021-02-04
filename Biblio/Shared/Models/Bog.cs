d4c9f5ef6a0580135055f8ee6e7458246013e7b6
Dennis Henrik Kristensen <Dennishk@protonmail.com>
2021-02-04 12:14:58 +0100

    GenericList checkpoint
    * Working connection through index->genericlist->markup file.
    
    Keep in mind the Bog class is a test class for this purpose ONLY!

---
+++ Biblio/Shared/Models/Bog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio.Shared.Models
+{
+    public class Bog
+    {
+        public int Id { get; set; }
+    }
+}
