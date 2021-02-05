746db9a564bf667680041e146b6a64ed24f29931
Dennis Henrik Kristensen <Dennishk@protonmail.com>
2021-02-05 10:39:16 +0100

    Generic view
    * Added the needed static class of properties for the generic component to work
    * Added the needed shared code to generic view

---
+++ Biblio/Client/Shared/SharedProperties.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblio.Client.Shared
+{
+    public static class SharedProperties
+    {
+        public enum BookTypes
+        {
+            Book,
+            Copy
+        }
+
+        public enum DisplayTypes
+        {
+            Card,
+            List,
+            Table
+        }
+    }
+}
