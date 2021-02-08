ad6510af79b47eb3dd7121bc0d4a5d38a0ca052a
Dennis Henrik Kristensen <Dennishk@protonmail.com>
2021-02-08 10:50:18 +0100

    Generic view
    * Added states to the sharedproperties
    * Added states to details and list view
    * Added the markup for the edits to the edit view
    * Added the edit component and logic to details view
    * Refactored the list view to directly use details view instead of the main page to use both. It is now controlled by the list view

--- Biblio/Client/SharedProperties.cs
+++ Biblio/Client/SharedProperties.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Biblio.Client.Shared
+namespace Biblio.Client
 {
     public static class SharedProperties
     {
@@ -20,6 +20,13 @@ namespace Biblio.Client.Shared
             Card,
             List,
             Table
+        }
+
+        public enum ViewState
+        {
+            ShowDetails,
+            ShowList,
+            Editing
         }
     }
 }
